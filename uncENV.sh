d6b504c4f94748fc8e1d6bfa89fff5324538733e
Administrator <matthew.r.kanke@gmail.com>
2016-12-08 16:57:59 -0500

    added images for manual

--- uncENV.sh
+++ uncENV.sh
@@ -1,10 +1,10 @@
 # set path
 
 #export PATH=/proj/.test/roach/miRNA/bin:./:$PATH
-export PYTHONPATH=/proj/.test/roach/miRNA/lib/python/
-export PATH=$PATH:/proj/.test/roach/miRNA/SHRiMP_2_2_2/bin
+#export PYTHONPATH=/proj/.test/roach/miRNA/lib/python/
+#export PATH=$PATH:/proj/.test/roach/miRNA/SHRiMP_2_2_2/bin
 
-module unload python
+#module unload python
 
 module load r/3.2.2
 module load bowtie/1.1.0
