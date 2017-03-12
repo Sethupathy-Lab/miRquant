c4cd7ff115540e8d2d6f128b0bd04d38dec19f46
Administrator <matthew.r.kanke@gmail.com>
2017-03-12 07:47:49 -0400

    update bedtools version

--- uncENV.sh
+++ uncENV.sh
@@ -2,11 +2,10 @@
 
 export PATH=~/.local/bin:$PATH
 export PYTHONPATH=~/.local/lib:$PYTHONPATH
-#export PATH=$PATH:/proj/.test/roach/miRNA/SHRiMP_2_2_2/bin
 
 module unload python
 
 module load r/3.2.2
 module load bowtie/1.1.0
-module load bedtools/2.25.0
+module load bedtools/2.26.0
 module load python/2.7.6
