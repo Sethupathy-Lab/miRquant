917dbe0f825e9ef8ce8735a6d47e73a0432d9a5b
Matthias <Matt.Kanke@gmail.com>
2018-01-31 10:07:50 -0500

    added cornell environment for sourcing

---
+++ cornell_mirquantENV.sh
@@ -0,0 +1,3 @@
+export PATH=/home/pr46_0001/shared/bin/:/programs/SHRiMP_2_2_3/bin/:$PATH
+export PYTHONPATH=/home/pr46_0001/shared/lib/:$PYTHONPATH
+export R_LIBS=/home/pr46_0001/shared/R_libs:$R_LIBS
