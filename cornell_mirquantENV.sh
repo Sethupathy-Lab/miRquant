3e742c8c999c0859e6d73280df161b1d56066a8e
Matthias <Matt.Kanke@gmail.com>
2018-05-14 09:43:58 -0400

    add bedtools to cornell env

--- cornell_mirquantENV.sh
+++ cornell_mirquantENV.sh
@@ -1,3 +1,3 @@
-export PATH=/home/pr46_0001/shared/bin/:/programs/R-3.4.2/bin:/programs/SHRiMP_2_2_3/bin/:$PATH
+export PATH=/home/pr46_0001/shared/bin/:/programs/R-3.4.2/bin:/programs/SHRiMP_2_2_3/bin/:/programs/bedtools2-2.26.0/bin:$PATH
 export PYTHONPATH=/home/pr46_0001/shared/lib/:$PYTHONPATH
 export R_LIBS=$R_LIBS:/home/pr46_0001/shared/R_libs
